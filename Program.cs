9f061540708d5ca8ee3e4cb0893196dd3f0b9448
Isaias Vasconcelos <isaiaskeyboards@gmail.com>
2024-04-16 23:19:58 -0300

    Remove Cors

--- Program.cs
+++ Program.cs
@@ -4,10 +4,4 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.InitRoutes();
-
-app.UseCors(x => {
-  x.AllowAnyHeader();
-  x.AllowAnyMethod();
-  x.AllowAnyOrigin();
-});
 app.Run();
