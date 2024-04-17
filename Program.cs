3cd8549e1ac9fcfa269b2e828a1e8d42b032b010
Isaias Vasconcelos <isaiaskeyboards@gmail.com>
2024-04-17 00:32:58 -0300

    Add Cors

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
 using FrasesMotivacionais.Routes;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options 
+  => options.AddPolicy(name: "_police", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
 var app = builder.Build();
+app.UseCors("_police");
 
 app.InitRoutes();
 app.Run();
